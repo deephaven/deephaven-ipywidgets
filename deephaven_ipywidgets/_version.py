9408db370a00d8507e1b04649de43d14c6b20aba
mikebender <mikebender@deephaven.io>
2022-09-06 12:15:31 -0400

    Change version to v0.1.0
    We only published v0.1.0dev, so we should actually publish v0.1.0

--- deephaven_ipywidgets/_version.py
+++ deephaven_ipywidgets/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Mike Bender.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 1)
+version_info = (0, 1, 0)
 __version__ = ".".join(map(str, version_info))
