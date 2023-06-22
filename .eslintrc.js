9ac74e1b431e0f2d079681c707d30f39405dc5bf
Joe Numainville <josephnumainville@deephaven.io>
2023-06-22 15:02:51 -0500

    fixing eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: ['@deephaven/eslint-config'],
+  parserOptions: {
+    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
 };
